10302afce76066af78dc4b9b122e2d427ee6705c
Chen Cai <chencai.math+1@gmail.com>
2022-03-03 14:46:04 -0800

    add output.md

--- README.md
+++ README.md
@@ -8,4 +8,7 @@
 * networkx              2.4
 
 ## Test
-python sparsenet/test/gen_test.py
+download the processed data [here](https://drive.google.com/drive/folders/1WMYebXwU7bVRWTW33BIx-sqAZV3UFFfX?usp=sharing)
+and set up directory in sparsenet/util/dir_util.py accordingly
+
+run ```python sparsenet/test/gen_test.py```
--- sparsenet/test/gen_test.py
+++ sparsenet/test/gen_test.py
@@ -123,7 +123,7 @@ class tester:
     def otherloss_test(self):
         """ test all loukas's datasets """
         datasets = ['ws', ]
-        train_indices =  '0,1,'# '0,1,2,3,4,'
+        train_indices =  '0,1,2,3,'# '0,1,2,3,4,'
         test_indices = '5,6,7,8,9,10,11,' # '5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,' # '10,11,12,13,14,15,16,17,18,19,' # '5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,' # '5,6,7,8,9,10,11,12,13,14,15,16'
         methods = self.method # [ ]  # ['heavy_edge', 'variation_edges',  'algebraic_JC', 'affinity_GS']
 
