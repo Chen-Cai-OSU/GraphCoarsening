4a75d4e83fe86f7d74787426e635f45b78a6a751
Chen Cai <chencai.math+1@gmail.com>
2022-03-03 14:04:54 -0800

    remove some files

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 # https://github.com/rusty1s/pytorch_geometric
-pip install torch-scatter==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-cluster==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-spline-conv==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-geometric
+# pip install torch-scatter==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+# pip install torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+# pip install torch-cluster==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+# pip install torch-spline-conv==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+# pip install torch-geometric
+
+TORCH='1.4.0'
+CUDA='cpu'
+# pip install torch-scatter -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+# pip install torch-geometric
